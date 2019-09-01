d0d1d56968d62120d65ac347b8d3521c4ce09419
denhag <thegboxer@gmail.com>
2019-09-01 11:50:14 +0200

    Revert "Attempt to save graphcoin"
    This reverts commit 82298955c6345fe2a4813ba1f0db26d0d04ed1b4.

--- src/miner.cpp
+++ src/miner.cpp
@@ -603,7 +603,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
             }
 
             if (chainActive.Tip()->nHeight < Params().LAST_POW_BLOCK()) {
-                MilliSleep(50);
+                MilliSleep(5000);
                 continue;
             }
 
@@ -619,7 +619,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
                         fMintableCoins = pwallet->MintableCoins();
                     }
                 }
-                MilliSleep(50);
+                MilliSleep(5000);
                 if (!fGenerateBitcoins && !fProofOfStake)
                     continue;
             }
@@ -628,7 +628,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
             {
                 if (GetTime() - mapHashedBlocks[chainActive.Tip()->nHeight] < max(pwallet->nHashInterval, (unsigned int)1)) // wait half of the nHashDrift with max wait of 3 minutes
                 {
-                    MilliSleep(50);
+                    MilliSleep(5000);
                     continue;
                 }
             }
